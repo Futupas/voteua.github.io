05c2daef33583a620a69bcd83992139cb05e1f96
Alex Paskal <alexpaskal@i.ua>
2016-08-16 15:58:05 +0300

    Vorking good!

--- Index/Content/js/app.js
+++ Index/Content/js/app.js
@@ -6,23 +6,25 @@ app.run(function ($rootScope, $firebaseObject) {
     console.log('--App.run');
     var ref = new Firebase("https://voteua-d239d.firebaseio.com");
     //$rootScope.data = { votes: [{ answers: [{}] }] };
-    $rootScope.currentvote = {};
-    $rootScope.currentvotename = null;
     $rootScope.data = {};
     obj = $firebaseObject(ref);
     obj.$bindTo($rootScope, 'data');
-    $rootScope.selectedanyvote = false;
     //$rootScope.$watch('data', function (newValue, oldValue) {
     //    $rootScope.$apply();
     //});
     obj.$loaded(function () {
         $rootScope.SetVoteOnPage(location.hash, true);
     });
-    $rootScope.hashcng = function () {
-        $rootScope.SetVoteOnPage(location.hash, true);
-    }
-    window.onhashchange = $rootScope.hashcng;
-    
+});
+
+app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
+
+    $scope.selectedanyvote = false;
+    $scope.currentvote = {};
+    $rootScope.curvote = {};
+    $scope.currentvotename = null;
+
+
     $rootScope.GetVote = function (votename, withhash) {
         var find = null;
         for (var i = 0; i < $rootScope.data.votes.length; i++) {
@@ -43,11 +45,11 @@ app.run(function ($rootScope, $firebaseObject) {
         for (var i = 0; i < $rootScope.data.votes.length; i++) {
             if (withhash == true) {
                 if (('#' + $rootScope.data.votes[i].nick) == votename) {
-                    find = i
+                    find = i;
                 }
             } else {
                 if ($rootScope.data.votes[i].nick == (votename)) {
-                    find = i
+                    find = i;
                 }
             }
         }
@@ -66,52 +68,58 @@ app.run(function ($rootScope, $firebaseObject) {
         return find;
     }
 
-
     $rootScope.SetVoteOnPage = function (votename, hashash) {
         var curr_vote = $rootScope.GetVote(votename, hashash);
         if (curr_vote !== null) {
-            $rootScope.selectedanyvote = true;
-            $rootScope.currentvote.name  = curr_vote.name;
-            $rootScope.currentvote.question = curr_vote.question;
-            $rootScope.currentvote.nick = curr_vote.nick;
-            $rootScope.currentvote.answers = curr_vote.answers;
+            $rootScope.curvote = curr_vote;
+            $scope.selectedanyvote = true;
+            $scope.currentvote.name  = curr_vote.name;
+            $scope.currentvote.question = curr_vote.question;
+            $scope.currentvote.nick = curr_vote.nick;
+            $scope.currentvote.answers = curr_vote.answers;
 
             var str = curr_vote.nick;
 
-            $rootScope.currentvotename = str;
+            $scope.currentvotename = str;
 
             if ($.cookie(str) != null && $.cookie(str) != 'null') {
-                $rootScope.activeAnsw = parseInt($.cookie(str));
+                $scope.activeAnsw = parseInt($.cookie(str));
             }
             else {
-                $rootScope.activeAnsw = null;
+                $scope.activeAnsw = null;
             }
 
             $rootScope.$apply();
+            $scope.$apply();
         }
     }
 
-});
+    $rootScope.hashcng = function () {
+        $rootScope.SetVoteOnPage(location.hash, true);
+    }
+    window.onhashchange = $rootScope.hashcng;
 
-app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
+
+
     $scope.showVote = function (nickname) {
         location.hash = nickname;
     }
     $scope.VoteClick = function (index) {
-        var str = $scope.currentvoteindex.toString();
+        var str = $rootScope.curvote.nick;
+        var idvote = $rootScope.GetVoteIndex(str, false);
         if ($.cookie(str) == null || $.cookie(str) == 'null') {
-            $rootScope.data.votes[$scope.currentvoteindex].answers[index].count++;
+            $rootScope.data.votes[idvote].answers[index].count++;
             $.cookie(str, index.toString(), {/*expires: 366, */path: '/'});
             $scope.activeAnsw = index;
         }
         else if (parseInt($.cookie(str)) != index) {
-            $rootScope.data.votes[$scope.currentvoteindex].answers[parseInt($.cookie(str))].count--;
-            $rootScope.data.votes[$scope.currentvoteindex].answers[index].count++;
+            $rootScope.data.votes[idvote].answers[parseInt($.cookie(str))].count--;
+            $rootScope.data.votes[idvote].answers[index].count++;
             $.cookie(str, index.toString(), {/*expires: 366, */path: '/' });
             $scope.activeAnsw = index;
         }
         else if (parseInt($.cookie(str)) == index) {
-            $rootScope.data.votes[$scope.currentvoteindex].answers[index].count--;
+            $rootScope.data.votes[idvote].answers[index].count--;
             $.cookie(str, null);
             $scope.activeAnsw = null;
         }
