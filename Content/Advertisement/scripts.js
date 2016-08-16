a031a94d5df32b2cc371eb49736c9c4fc9e791c0
Alex Paskal <alexpaskal@i.ua>
2016-08-16 17:37:10 +0300

    My ad

---
+++ Content/Advertisement/scripts.js
@@ -0,0 +1,28 @@
+﻿var a = false;
+
+$('#body').mouseenter(function () {
+    $('#modal').modal({
+        keyboard: false,
+        backdrop: false,
+        show: true
+    });
+    a = true;
+});
+
+$('#body').mouseleave(function () {
+    $('#modal').modal('hide');
+    a = false;
+});
+
+$('#body').click(function () {
+    a = !a;
+    if (a) {
+        $('#modal').modal({
+                keyboard: false,
+                backdrop: false,
+                show: true
+            });
+    } else {
+        $('#modal').modal('hide');
+    }
+});
