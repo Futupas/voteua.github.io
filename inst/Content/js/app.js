6903467b97cc67927d3c9cebf8f2d80ade2eda47
Alex Paskal <alexpaskal@i.ua>
2016-08-16 18:53:38 +0300

    Add instruction (ua, en)

---
+++ inst/Content/js/app.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('App', []);
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.inst_lang = './english.html';
+    $scope.SetEngLang = function () {
+        $scope.inst_lang = './english.html';
+        $('#SetUkrLang').removeClass('active');
+        $('#SetEngLang').addClass('active');
+    }
+    $scope.SetUkrLang = function () {
+        $scope.inst_lang = './ukrainian.html';
+        $('#SetEngLang').removeClass('active');
+        $('#SetUkrLang').addClass('active');
+    }
+});
---
+++ inst/Content/js/view.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    SetContentCss();
+    $('#content').niceScroll({
+        cursorcolor: '#000000',
+        cursorwidth: '6px',
+        cursorborder: '0px',
+        cursorborderradius: '3px'
+    });
+});
+$(window).resize(function () {
+    SetContentCss();
+});
+
+function SetContentCss() {
+    $('#header')
+        .css('width', (document.documentElement.clientWidth - 10))
+        .css('height', 50);
+    $('#content')
+        .css('width', (document.documentElement.clientWidth - 10))
+        .css('height', (document.documentElement.clientHeight - parseInt($('#header').css('height')) - 15));
+}
